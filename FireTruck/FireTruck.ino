12f9885fd801e00940c8431128f30312ec052760
TEK0MT <mohamedmttarekmt@gmail.com>
2024-07-31 03:37:33 +0300

    Updates

--- FireTruck/FireTruck.ino
+++ FireTruck/FireTruck.ino
@@ -3,10 +3,10 @@
 #define FLAMELEFT 4
 #define MQ2 12
 
-#define M1  7
-#define M2  8
-#define M3  5
-#define M4  6
+#define M1  8
+#define M2  7
+#define M3  6
+#define M4  5
 #define Pump 13
 
 void move_forward(){
@@ -23,15 +23,15 @@ void move_Right(){
 }
 void move_Left(){
   digitalWrite(M3,LOW);
-  digitalWrite(M4,HIGH);
+  digitalWrite(M2,LOW);
   digitalWrite(M1,HIGH);
-  digitalWrite(M3,LOW);
+  digitalWrite(M4,HIGH);
 }
 void move_Stop(){
   digitalWrite(M3,LOW);
   digitalWrite(M4,LOW);
   digitalWrite(M1,LOW);
-  digitalWrite(M3,LOW);
+  digitalWrite(M2,LOW);
 }
 void fire_off(){
 digitalWrite(Pump,HIGH);
@@ -52,9 +52,9 @@ Serial.begin(9600);
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(9,LOW);
+ /* digitalWrite(9,LOW);
   digitalWrite(8,LOW);
-  digitalWrite(10,LOW);
+  digitalWrite(10,LOW);*/
   int righ = digitalRead(FLAMERIGHT);
   int left = digitalRead(FLAMELEFT);
   int center = digitalRead(FLAMECENTER);
@@ -65,13 +65,16 @@ void loop() {
     fire_off();
     }
   }
-  else if(righ == LOW && center == HIGH && left == HIGH){
+  else if(righ == HIGH &&  left == LOW){
     move_Right();
   }
-  else if(left == LOW && righ == HIGH && center == HIGH){
+  else if(left == HIGH && righ == LOW ){
     move_Left();
   }
-  else if (center == LOW){
+  else if (center == HIGH && righ == LOW && left == LOW){
     move_forward();
+  }
+  else{
+    move_Stop();
   }
 }
